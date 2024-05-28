3f65a6ada09abc944005b8dcf2ef202385f23f0f
muhammet2005 <muxammetamatov205@gmail.com>
2024-05-28 19:03:16 +0700

    header input

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 /** @format */
+import { useState } from "react"
 import { FiSearch } from "react-icons/fi"
 import { IoNotificationsOutline } from "react-icons/io5"
 import { LuUser } from "react-icons/lu"
@@ -6,6 +7,11 @@ import { NavLink } from "react-router-dom"
 import Logo from "../../assets/img/logo.svg"
 import "./Header.scss"
 const Header = () => {
+	const [isSearchIcon, setIsSearchIcon] = useState<boolean>(false)
+
+	const handleSearchIconClick = () => {
+		setIsSearchIcon(!isSearchIcon)
+	}
 	return (
 		<div id="header">
 			<div className="header__inner container">
@@ -21,7 +27,12 @@ const Header = () => {
 						/>
 					</NavLink>
 
-					<nav className="header__menu ">
+					<nav
+						className="header__menu "
+						style={{
+							display: isSearchIcon ? "none" : "flex",
+						}}
+					>
 						<ul className="header__menu__list">
 							<li className="header__menu-item">
 								<NavLink className="header__menu-link" to="/">
@@ -52,21 +63,34 @@ const Header = () => {
 					</nav>
 				</div>
 
-				<div className="header__right__block">
-					<button>
-						<FiSearch />
-					</button>
+				<div
+					className="header__right__block  pl-[10%]"
+					style={{
+						width: isSearchIcon ? "90%" : "30%",
+					}}
+				>
+					<input
+						style={{
+							width: isSearchIcon ? "200%" : "0%",
+						}}
+						height={isSearchIcon ? "60px" : "0px"}
+						className="header__search input"
+					/>
 
-					<input type="hidden" className="header__search input" />
-					<button className="header__button button button__buy">
-						buy for 50$
-					</button>
-					<button>
-						<IoNotificationsOutline />
-					</button>
-					<button className="border">
-						<LuUser />
-					</button>
+					<div className="w-[100%] flex  gap-4 ml-0  justify-end">
+						<button onClick={handleSearchIconClick}>
+							<FiSearch />
+						</button>
+						<button className="header__button button button__buy">
+							buy for 50$
+						</button>
+						<button>
+							<IoNotificationsOutline />
+						</button>
+						<button className="border">
+							<LuUser />
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
